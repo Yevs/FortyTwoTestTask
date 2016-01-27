d394f73fe383291aa17cdde0d2623129dbfde04f
Yevs <yevhen.yevsyuhov@gmail.com>
2016-01-27 14:08:17 +0200

    added basic test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
 
+class IndexTest(TestCase):
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+    def test_indexpage(self):
+        """
+        Tests whether GET to '/' returns response with 200 status code"""
+        
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
