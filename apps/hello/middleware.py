80223f7a18a41a9186eee5e416b866d8454cae01
Yevs <yevhen.yevsyuhov@gmail.com>
2016-01-30 10:33:07 +0200

    ticket:3 removed logging requests for static and restricted number of requests in table to 10

--- apps/hello/middleware.py
+++ apps/hello/middleware.py
@@ -5,7 +5,9 @@ from django.utils import timezone
 class RequestLoggerMiddleware(object):
 
     def process_request(self, request):
-        if '/api/' not in request.path:
+        if '/api/' not in request.path and\
+           '/static/' not in request.path and\
+           'favicon.ico' not in request.path:
             RequestLog(datetime=timezone.now(),
                        method=request.method, path=request.path).save()
         return None
