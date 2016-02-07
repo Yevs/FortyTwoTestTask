84f0e024f585d770406972255a94a2585bf044aa
Yevs <yevhen.yevsyuhov@gmail.com>
2016-02-07 12:22:14 +0200

    ticket:13 quickfixed flake8 errors

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Person, RequestLog
 
 
-
 class RequestAdmin(admin.ModelAdmin):
 
     list_display = ('path', 'method', 'datetime', 'priority', )
