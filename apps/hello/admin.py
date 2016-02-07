227a9f65be5f038090330dc16f492720e9005b2d
Yevs <yevhen.yevsyuhov@gmail.com>
2016-02-07 12:18:44 +0200

    ticket:13 added RequestAdmin for RequestLog

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Person, RequestLog
 
 
+
+class RequestAdmin(admin.ModelAdmin):
+
+    list_display = ('path', 'method', 'datetime', 'priority', )
+    list_filter = ('priority', )
+
 admin.site.register(Person)
-admin.site.register(RequestLog)
+admin.site.register(RequestLog, RequestAdmin)
